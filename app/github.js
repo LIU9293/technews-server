c5dcdfb603ca72cd0bbd9fa529ebe1c25473dfbc
liu9293 <mylock.kai@gmail.com>
2017-04-04 17:53:25 +0800

    add markdown string

--- app/github.js
+++ app/github.js
@@ -20,10 +20,18 @@ function getReadme(user, repoName, callback){
   const url = `https://api.github.com/repos/${user}/${repoName}/readme`;
   fetch(url)
     .then(res => res.json())
-    .then(json => callback(null, {
-      success: true,
-      data: json
-    }))
+    .then(json => {
+      const Base64Content = json.content;
+      const MarkdownString = Buffer.from(Base64Content, 'base64').toString('utf8');
+      //const filePath = `https://raw.githubusercontent.com/${user}/${repoName}/master`;
+      //MarkdownString = MarkdownString.replace(/<img([^>]*)\ssrc=(['"])(?:[^\2\/]*\/)*([^\2]+)\2/gi, `<img$1 src=$2${filePath}/$3$2`);
+      callback(null, {
+        success: true,
+        data: Object.assign({}, json, {
+          markdown: MarkdownString
+        })
+      });
+    })
     .catch(err => callback(null, {
       success: false,
       message: err.toString()
